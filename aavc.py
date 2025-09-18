8cdb896109269bc1bf29d01298f506793b58b2e0
ardainn <100212797+ardainn@users.noreply.github.com>
2025-09-18 11:46:47 +0300

    added flag "PROTEIN_LENGTH_CHANGES_IN_NON-REPEAT_REGION"

--- aavc.py
+++ aavc.py
@@ -1705,6 +1705,9 @@ class ACMG:
                 else:
                     self.criteria["PM4"] = "PM4_M"
                     self.flags.append("CRITICAL_REPEAT")
+			else:
+				self.criteria["PM4"] = "PM4_M"
+                self.flags.append("PROTEIN_LENGTH_CHANGES_IN_NON-REPEAT_REGION")	
         else:
             self.criteria["PM4"] = 0
 
